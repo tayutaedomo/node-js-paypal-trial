a6e9813e083064220d0fbff86da02e568e6ec26f
tayutaedomo <tayutaedomo@gmail.com>
2016-02-20 18:59:38 +0900

    Add paypal initialization code

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ Install node-dev.
 $ npm install -g node-dev
 ```
 
+Get PayPal sandbox information. See following:
+- https://developer.paypal.com/docs/classic/lifecycle/ug_sandbox/
+
+Set environment variables.
+```
+export PAYPAL_MODE=sandbox
+export PAYPAL_CLIENT_ID=<your client id>
+export PAYPAL_CLIENT_SECRET=<your client secret>
+```
+
 git clone and start app.
 ```
 $ git clone git@github.com:tayutaedomo/node-paypal-subscription.git
--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,8 +1,25 @@
+var paypal = require('paypal-rest-sdk');
 var express = require('express');
 var router = express.Router();
 
+//
+// Initialize PayPal
+//
+paypal.configure({
+  mode: process.env.PAYPAL_MODE || '',
+  client_id: process.env.PAYPAL_CLIENT_ID || '',
+  client_secret: process.env.PAYPAL_CLIENT_SECRET || ''
+});
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  paypal.generateToken(function(err, token) {
+    res.render('subscriptions/index', {
+      'title': 'PayPal Initialization Check',
+      token: token || '',
+      error: err || {}
+    });
+  });
 });
 
 module.exports = router;
