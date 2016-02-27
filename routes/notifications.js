3228af8996b98ea7f72093ffe3bffb2eabc0916a
tayutaedomo <tayutaedomo@gmail.com>
2016-02-27 23:12:44 +0900

    Add logging code

--- routes/notifications.js
+++ routes/notifications.js
@@ -84,6 +84,7 @@ router.get('/webhook_event_types', function(req, res, next) {
 router.post('/webhook/listener', function(req, res, next) {
   console.log(req.body);
   console.log(req.body.id);
+  console.log(req.body.resource.id);
   res.send('OK');
 });
 
